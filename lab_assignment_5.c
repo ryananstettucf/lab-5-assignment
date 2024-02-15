f32429bb343ede2443527c55a0c5b9b14e489e0c
Ryan Anstett <ry168247@ucf.edu>
2024-02-15 16:12:04 -0500

    Lab 5 assignment

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -9,22 +9,36 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+	int size = 0;
+	node * current = head;
+	while (current != NULL ) {
+		size++;
+		current = current -> next;
+	}
+	return size;
 
-   return (len);
 }
 
 // parses the string in the linkedList
 //  if the linked list is head -> |a|->|b|->|c|
 //  then toCString function wil return "abc"
 char* toCString(node* head)
-{
+{ 
+	int size = length(head);
+	char* str = (char*) malloc(sizeof(char) * (size +1));
+	if(!str) {
+		printf("Memory allocation failed.\n");
+		exit(1);
+	}
+	node* current = head;
+	int i = 0;
+	while (current != NULL ) {
+		str[i] = current -> letter;
+		current = current -> next;
+		i++;
+	}
+	str[i] = '\0';
+	return str;
 }
 
 // inserts character to the linkedlist
@@ -33,11 +47,37 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+	node* newNode = (node *) malloc(sizeof(node));
+
+	if(!newNode) {
+		printf("Memory allocation failed.\n");
+		exit(1);
+	}
+	newNode -> letter = c;
+	newNode -> next =NULL;
+	if (*pHead == NULL) {
+		*pHead = newNode;
+	}
+	else {
+		node* current = *pHead;
+		while (current -> next != NULL) {
+			current =current -> next;
+		}
+		current -> next = newNode;
+	}
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+	node* current = *pHead;
+	node* next;
+	while (current != NULL) {
+		next = current -> next;
+		free (current);
+		current = next;
+	}
+	*pHead = NULL;
 }
 
 int main(void)
@@ -46,12 +86,17 @@ int main(void)
 	node* head = NULL;
 	char* str;
 	char c;
-	FILE* inFile = fopen("input.txt","r");
+	FILE* inFile = fopen("./input.txt","r");
+	if(inFile == NULL){
+		printf("Failed to open file.\n");
+		return 1;
+	}
 
 	fscanf(inFile, " %d\n", &numInputs);
 	
 	while (numInputs-- > 0)
 	{
+		
 		fscanf(inFile, " %d\n", &strLen);
 		for (i = 0; i < strLen; i++)
 		{
